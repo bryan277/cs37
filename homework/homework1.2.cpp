55527f07c6c6b6d0dbf7b0e3020cc7c21b24084b
hfvos90 <hfvos90@gmail.com>
2019-01-24 17:51:20 -0800

    #

--- homework/homework1.2.cpp
+++ homework/homework1.2.cpp
@@ -5,24 +5,31 @@
 
 #include<iostream>
 #include <iomanip>
+using namespace std;
+
+#define MAX_NAME_LENGTH     50
+#define MAX_ADDRESS_LENGTH  100
 
 int main()
 {
-   std::string name, address;
+   char name[MAX_NAME_LENGTH],address[MAX_ADDRESS_LENGTH];
    int age;
    float salary;
 
-   std::cout << "Enter your name: ";
-   std::getline(std::cin, name);
+   cout << "Enter your name: ";
+   cin.getline(name,MAX_NAME_LENGTH);
 
-   std::cout << "Enter your age: ";
-   std::cin >> age;
+   cout << "Enter your age: ";
+   cin >> age;
+   cin.ignore();
 
-   std::cout << "What is your address: ";
-   std::getline(std::cin, address);  //What is your address: Enter your salary:
+   cout << "Enter address: ";
+   cin.getline(address,MAX_ADDRESS_LENGTH);
 
    std::cout << "Enter your salary: ";
    std::cin >> salary;
 
    std::cout << std::left << std::setw(20) << name << std::left << std::setw(20) << address << std::right << std::setw(5) << age << std::right << std::setw(10) << printf("%.2f", salary) << std::endl;
+
+   return 0;
 }
